17d51339678d19a8adc464430d6c04464affad42
TANISHA SINGHAL <88341606+Tanisha01-small@users.noreply.github.com>
2023-02-13 18:58:25 +0530

    change2

--- editthis.js
+++ editthis.js
@@ -1,24 +1,22 @@
 var site = {
       /* Your name */
     name : 'Tanisha Singhal',
-    job: 'Front-End Developer and Business Analyst',
+    job: 'Front-End Developer and Business Analyst Enthusiast',
        /* This decription is used both in meta tag and about page */
     description: `Hi there! I am Tanisha Singhal from Delhi, India.
     I am currently pursuing Bachelor of Technology in Engineering Physics.
-    I ❤️ to design Websites, doing prograaming stuff, Managing teams and i love reading blogs.`,
+    I ❤️ to design Websites, doing programming stuff, Managing teams and i love reading blogs.`,
     /* Change your skills and title accodingly */
     skills_title: ['Web Development','Programming'],
-    skills: [['HTML', 'CSS', 'JS', 'ReactJS','MYSQL'], ['Photoshop', 'XD','Figma'],['JAVA','Python3','Javascript']],
+    skills: [['HTML', 'CSS', 'JS', 'ReactJS','MYSQL'],['JAVA','Python3','Javascript']],
     skillp:[[100, 80 ,100, 60, 80], [50, 60, 50], [70,80,90,90]],
     social: {
         /* Add your social media links here */
         github: 'https://github.com/Tanisha01-small',
-        twitter: 'https://twitter.com/adithyapaib',
         linkedin: 'http://www.linkedin.com/in/tanisha01singhal',
-        mail: 'mailto: email@gmail.com',
-        instagram: 'https://instagram.com'
+        mail: 'mailto: tanishasinghal0101@gmail.com',
     },
-    form: 'https://formsubmit.co/email@gmail.com',
+    form: 'https://forms.gle/WpzVyKK9b7UsXfY8A',
     /* DONT FOR GET TO ACTIVATE UR EMAIL AFTER SETTING UP THE FORM TO RECEIVE EMAILS */
     projects: [
         ["https://google.com", 'https://github.com/adithyapaib/portfolio-template',"https://github.com/adithyapaib/portfolio-template", "https://github.com/adithyapaib/portfolio-template"],
